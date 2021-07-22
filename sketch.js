c306edaa680deb72e4ea52cee9a4e8ee96338c4c
Ashutosh2006-alt <79429930+Ashutosh2006-alt@users.noreply.github.com>
2021-07-22 14:55:12 +0530

    code

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var backgroundImg;
+
+function preload() {
+  getBackgroundImg();
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  if (backgroundImg)
+    background(backgroundImg);
+
+}
+
+async function getBackgroundImg() {
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+  var dateTime = responseJSON.datetime;
+  var hour = dateTime.slice(11, 13);
+  if (hour >= 0 && hour <= 18) {
+    bg = "images/1.jpg";
+  } else if(hour >= 18 && hour <= 24) {
+    bg = "images/2.jpg";
+  }
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
+
+}
